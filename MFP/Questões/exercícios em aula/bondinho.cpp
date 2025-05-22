72e32ca63e03dc75ddba5ecf74c75f3a1fd10b20
sabiss <sabrinabezsilva@gmail.com>
2025-05-22 10:18:19 -0300

    questões

---
+++ MFP/Questões/exercícios em aula/bondinho.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a = 0;
+    int m = 0;
+
+    cin >>a;
+    cin >>m;
+
+    if(m+a>50){
+        cout<<"N";
+    }else{
+        cout<<"S";
+    }
+}
